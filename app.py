81abd57f65b4a76efa668bba16d68349058f8af4
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-05-18 12:44:27 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Software Ticket Dashboard", layout="wide")
+st.set_page_config(page_title="Software Tickets", layout="wide")
 
 st.title("🎫 Software Ticket Dashboard")
 
