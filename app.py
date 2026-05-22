eb363879e1cc67b4ef827273c2d7e1892e278d7e
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-05-22 10:44:52 +0530

    app.py
    Add an image to the Streamlit app.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#add image
+st.image("envision.png")
+
 # Page Configuration
 st.set_page_config(
     page_title="Software Ticket Dashboard",
