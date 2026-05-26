2948827de0f5e063c6e95aa201c3962a7eb6b022
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-05-26 14:33:10 +0530

    login_once.py

---
+++ login_once.py
@@ -0,0 +1,21 @@
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    context = browser.new_context()
+
+    page = context.new_page()
+
+    page.goto("https://yourcompany.service-now.com")
+
+    print("Login manually using Okta...")
+
+    input("Press ENTER after login completes...")
+
+    context.storage_state(path="auth.json")
+
+    print("Session saved!")
+
+    browser.close()
