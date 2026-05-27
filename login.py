8f4ebead66d40fe7ba7d9f52b3f999da0f5c45d0
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-05-27 16:51:43 +0530

    login.py

--- login.py
+++ login.py
@@ -1,21 +1,25 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
+
     browser = p.chromium.launch(
-         headless=False,
-         args=[
-             "--no-sandbox",
-             "--disable-dev-shm-usage",
-             "--disable-gpu",
-             "--disable-setuid-sandbox",
-             "--single-process"
+        headless=False,
+        args=[
+            "--no-sandbox",
+            "--disable-dev-shm-usage"
         ]
     )
+
     context = browser.new_context()
+
     page = context.new_page()
+
     page.goto("https://ee.envision-energy.com")
-    print("Login manually using Okta...")
-    input("Press ENTER after login completes...")
+
+    input("After login completes press ENTER...")
+
     context.storage_state(path="auth.json")
-    print("Session saved!")
+
+    print("auth.json created successfully")
+
     browser.close()
