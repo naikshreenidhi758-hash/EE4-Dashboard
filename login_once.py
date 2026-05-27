301802f86d2d71a23e083feba004a5a70ebbd631
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-05-27 10:19:58 +0530

    login_once.py

--- login_once.py
+++ login_once.py
@@ -1,7 +1,10 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(
+    headless=True,
+    args=["--no-sandbox", "--disable-dev-shm-usage"]
+)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://ee.envision-energy.com")
