07f41581c166fc57f8aa1d4100d407baa708099e
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-01 17:13:06 +0530

    case_processing.py

--- case_processing.py
+++ case_processing.py
@@ -1,1 +1,14 @@
+import streamlit as st
 import pandas as pd
+import plotly.express as px
+
+#page configuration
+st.page_config(
+ page_title="Case Processing Dashboard"
+  layout="wide"
+
+#title
+st.markdown(
+  "<h1 style="text-align:center;>INDIA Project-Case processing Status</h1>
+  unsafe_allow_html=True
+  
