259140edc32a70fb2a9c0b9beb690d8251e87c51
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-02 11:51:05 +0530

    case_processing.py

--- case_processing.py
+++ case_processing.py
@@ -2,19 +2,22 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-#page configuration
+# Page configuration
 st.set_page_config(
- page_title="Case Processing Dashboard",
-  layout="wide"
+    page_title="Case Processing Dashboard",
+    layout="wide"
 )
 
-#title
-st.title("INDIA Project-Case processing Status📋")
+# Title
+st.title("🇮🇳 INDIA Project - Case Processing Status 📋")
 
-#read from sharepoint
-df.load_sharepoint_data()
-site_url=https://envisionint.sharepoint.com/sites
+# SharePoint URLs
+site_url = "https://envisionint.sharepoint.com/sites"
 
-file_url=https://envisionint.sharepoint.com/:x:/r/sites
+file_url = "https://envisionint.sharepoint.com/:x:/r/sites"
+
+# Read Excel
 df = pd.read_excel(file_url)
+
+st.dataframe(df)
   
