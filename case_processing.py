332891b5a311c2d8923889675d1cb277bbb0209d
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-03 12:43:08 +0530

    case_processing.py

--- case_processing.py
+++ case_processing.py
@@ -94,7 +94,7 @@ with col2:
 
 #convert date columns
 df["start date"]=pd.to_datetime(df["start date"],errors="coerce")
-df["end time']=pd.to_datetime(df["end date"],"errors=coerce")
+df["end time"]=pd.to_datetime(df["end date"],"errors=coerce")
 
 #calculating days between start date and end date
 df["case_days"]=(df["end date"]-df["start date"]).dt.days
