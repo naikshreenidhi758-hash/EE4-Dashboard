bda205a1a266ea8db020c08a066c07b4d469ca85
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-03 12:41:20 +0530

    case_processing.py

--- case_processing.py
+++ case_processing.py
@@ -91,3 +91,48 @@ with col1:
 
 with col2:
     st.plotly_chart(fig_pie, use_container_width=True)
+
+#convert date columns
+df["start date"]=pd.to_datetime(df["start date"],errors="coerce")
+df["end time']=pd.to_datetime(df["end date"],*errors=coerce")
+
+#calculating days between start date and end date
+df["case_days"]=(df["end date"]-df["start date"]).dt.days
+
+#analyse
+pending_df=df[df["status"].str.lower()=="pending"].copy()
+
+pending_df["age_bucket"]=pd.cut(
+    pending_df["case_days"],
+    bins=[-1,7,15,float("inf")],
+    lables=["0-7 Days","8-15 Days",">15 Days"]
+)
+
+pending_summary=(
+    pending_df.groupby(
+        ["first engineer(india team)","age_bucket"]
+    )
+    .size()
+    .reset_index(name="count")
+    )
+
+#create stacked bar
+fig_pending=px.bar(
+    pending_summary,
+    x="firsat engineer(india team)",
+    y="count",
+    color="age_bucket",
+    text="count",
+    barmode="stack",
+    title="pending cases by age",
+    color_discrete_map={
+    "0-7 Days":"green",
+    "8-15 Days":"orange",
+    :>15 Days":"red"
+    }
+    )
+    st.plotly_chart(fig_pending,use_container_widtTrue)
+    
+
+
+
