0653a751dd11edca728ee30af47d95b858e926f5
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-10 12:04:43 +0530

    customer_service.py

--- customer_service.py
+++ customer_service.py
@@ -9,11 +9,13 @@ st.set_page_config(
     layout="wide"
 )
 
-excel_url = "https://envisionint-my.sharepoint.com/:x:/r/personal/nitheesh_ng_envision-energy_com/Documents/sn_customerservice_case.xlsx?d=w64b168e1f1bf4a74acefdd6145bf8aa4&csf=1&web=1&e=805A4H"
-
-df = pd.read_excel(excel_url)
+# LOAD EXCEL
+df = pd.read_excel(
+    "sn_customerservice_case.xlsx",
+)
 
 df.columns = df.columns.str.strip()
 
 # CLEAN COLUMN NAMES
 df.columns = df.columns.str.strip().str.lower()
+
