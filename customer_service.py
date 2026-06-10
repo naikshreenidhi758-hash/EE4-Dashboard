046818e104dd887aa686390fd43ebdb500264c3f
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-10 11:51:50 +0530

    customer_service.py

---
+++ customer_service.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+# PAGE CONFIG
+st.set_page_config(
+    page_title="Customer Service",
+    layout="wide"
+)
+
+st.title("Customer Service")
+
+# LOAD EXCEL
+df = pd.read_excel(
+    
+    
+
+df.columns = df.columns.str.strip()
+
+# CLEAN COLUMN NAMES
+df.columns = df.columns.str.strip().str.lower()
