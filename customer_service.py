aca5d12590280c964b66fa2c775ad869542b24a5
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-10 12:13:35 +0530

    customer_service.py
    Added functionality to count colored cells in the Excel sheet.

--- customer_service.py
+++ customer_service.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from openpyxl import load_workbook
 import pandas as pd
 import plotly.express as px
 
@@ -19,3 +20,31 @@ df.columns = df.columns.str.strip()
 # CLEAN COLUMN NAMES
 df.columns = df.columns.str.strip().str.lower()
 
+
+wb = load_workbook("sn_customerservice_case.xlsx")
+ws = wb["Infra EE4 Cases"]
+
+infra_count = 0
+universe_count = 0
+
+for row in ws.iter_rows(min_row=2):
+    cell = row[3]   # Short description column (adjust index)
+
+    color = cell.fill.fgColor.rgb
+
+    if color == "FF00FF00":  # Green
+        infra_count += 1
+    elif color == "FFFF0000":  # Red
+        universe_count += 1
+
+print("Infra:", infra_count)
+print("Universe:", universe_count)
+
+fig = px.pie(
+    team_summary,
+    names="Team",
+    values="Count",
+    title="Infra vs Universe"
+)
+
+st.plotly_chart(fig, use_container_width=True)
