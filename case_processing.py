daf22cb0246a595ed3365358b26729c40a737883
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-24 17:36:26 +0530

    case_processing.py

--- case_processing.py
+++ case_processing.py
@@ -187,30 +187,6 @@ filtered_df["first engineer(india team)"] = (
     .astype(str)
     .str.strip()
 )
-st.write("Selected Month:", selected_month)
-
-pending_month = filtered_df[
-    filtered_df["status"]
-    .astype(str)
-    .str.strip()
-    .str.lower()
-    .ne("closed")
-]
-
-st.write("Pending Cases in Selected Month:", len(pending_month))
-
-st.dataframe(
-    pending_month[
-        [
-            "first engineer(india team)",
-            "status",
-            "start date",
-            "end time"
-        ]
-    ]
-)
-
-st.write(filtered_df["status"].value_counts())
 
 filtered_df.loc[
     filtered_df["first engineer(india team)"] == "",
