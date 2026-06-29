e2a0278f1ebb56ac065f185767786f1a74e8ebd7
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-06-29 16:03:00 +0530

    case_processing.py
    Removed print statements for pending totals and counts.

--- case_processing.py
+++ case_processing.py
@@ -350,26 +350,6 @@ pending_summary = pending_summary[
     pending_summary["count"] > 0
 ]
 
-print("Total pending:", len(pending_df))
-print("Total in summary:", pending_summary["count"].sum())
-
-print(
-    pending_df["age_bucket"]
-    .value_counts(dropna=False)
-)
-
-print(
-    pending_df[
-        pending_df["age_bucket"].isna()
-    ][[
-    
-        "first engineer(india team)",
-        "start date",
-        "filled_date",
-       
-        
-    ]]
-)
 
 fig_pending = px.bar(
     pending_summary,
