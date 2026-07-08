76c4bff318d87b8a04e32b35146890584f51f306
naikshreenidhi758-hash <naikshreenidhi758@gmail.com>
2026-07-08 14:39:36 +0530

    incident_tracker.py
    Removed the expander for viewing total tickets and its associated dataframe display.

--- incident_tracker.py
+++ incident_tracker.py
@@ -116,20 +116,6 @@ if uploaded_file:
     col1.metric("Total Tickets", total_tickets)
     col2.metric("Open Tickets", open_tickets)
     
-with st.expander(f"View  {total_tickets} total_tickets"):
-   total_tickets_list = df[
-        df["status"]
-        .astype(str)
-        .str.strip()
-        .str.lower()
-        .ne("closed")
-    ]
-
-st.dataframe(
-    total_tickets_list,
-    use_container_width=True
-)
-    
 col3.metric("Closed Tickets", closed_tickets)
 
 # Ticket Data
